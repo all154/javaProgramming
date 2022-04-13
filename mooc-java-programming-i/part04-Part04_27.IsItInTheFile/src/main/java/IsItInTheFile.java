d6b5f96983ee822ae3c2f8cfc19eb65bfa740036
Aloisio Valerio Jr <aloisio.valeriojr@gmail.com>
2022-04-13 11:45:47 -0300

    feat: IsItInTheFile.java

--- mooc-java-programming-i/part04-Part04_27.IsItInTheFile/src/main/java/IsItInTheFile.java
+++ mooc-java-programming-i/part04-Part04_27.IsItInTheFile/src/main/java/IsItInTheFile.java
@@ -1,5 +1,6 @@
 
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class IsItInTheFile {
@@ -12,6 +13,21 @@ public class IsItInTheFile {
 
         System.out.println("Search for:");
         String searchedFor = scanner.nextLine();
+        
+        ArrayList<String> list = new ArrayList<>();
+        try (Scanner scan = new Scanner(Paths.get(file))) {
+            while (scan.hasNextLine()) {
+                list.add(scan.nextLine());
+            }
+        } catch (Exception e) {
+            System.out.println("Reading the file " + file + " failed.");
+        }
+        
+        if (list.contains(searchedFor)) {
+            System.out.println("Found!");
+        } else {
+            System.out.println("Not found.");
+        }
 
     }
 }
