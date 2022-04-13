569fe13b3d893e39e12a6dd86e37df0e1c077f99
Aloisio Valerio Jr <aloisio.valeriojr@gmail.com>
2022-04-13 10:11:55 -0300

    feat: add NumberOfStrings

--- part04-Part04_21.NumberOfStrings/src/main/java/NumberOfStrings.java
+++ part04-Part04_21.NumberOfStrings/src/main/java/NumberOfStrings.java
@@ -5,6 +5,19 @@ public class NumberOfStrings {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int i = 0;
+        
+        while (true) {
+            String text = scanner.nextLine();
+            
+            if (text.equals("end")) {
+                break;
+            }
+            
+            i++;
+        }
+        
+        System.out.println(i);
 
     }
 }
