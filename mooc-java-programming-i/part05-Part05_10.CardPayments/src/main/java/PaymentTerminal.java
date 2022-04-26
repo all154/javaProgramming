c5445c26a51485858793ae426191d01a8084d53e
Aloisio Valerio Jr <aloisio.valeriojr@gmail.com>
2022-04-26 12:45:55 -0300

    feat: eatAffordably(double payment)

--- mooc-java-programming-i/part05-Part05_10.CardPayments/src/main/java/PaymentTerminal.java
+++ mooc-java-programming-i/part05-Part05_10.CardPayments/src/main/java/PaymentTerminal.java
@@ -8,20 +8,22 @@ public class PaymentTerminal {
 
 
     public PaymentTerminal() {
-        // register initially has 1000 euros of money
+        this.money = 1000;
+        this.affordableMeals = 0;
+        this.heartyMeals = 0;
     }
 
     public double eatAffordably(double payment) {
-        // an affordable meal costs 2.50 euros
-        // increase the amount of cash by the price of an affordable mean and return the change
-        // if the payment parameter is not large enough, no meal is sold and the method should return the whole payment
-        return -1;
+        if (payment >= 2.5) {
+            this.money += 2.5;
+            this.affordableMeals += 1;
+            return payment - 2.5;
+        } else {
+            return payment;
+        }
     }
 
     public double eatHeartily(double payment) {
-        // a hearty meal costs 4.30 euros
-        // increase the amount of cash by the price of a hearty mean and return the change
-        // if the payment parameter is not large enough, no meal is sold and the method should return the whole payment
         return -1;
     }
 
