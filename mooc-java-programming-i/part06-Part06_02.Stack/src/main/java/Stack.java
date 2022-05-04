2074fb97f326850ab8a90fdeaf82a017445e156c
Aloisio Valerio Jr <aloisio.valeriojr@gmail.com>
2022-05-04 11:00:54 -0300

    feat: Stack constructor

--- mooc-java-programming-i/part06-Part06_02.Stack/src/main/java/Stack.java
+++ mooc-java-programming-i/part06-Part06_02.Stack/src/main/java/Stack.java
@@ -1,13 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author alois
- */
+import java.util.ArrayList;
+
+
 public class Stack {
-    
+    private ArrayList<String> stack;
+
+    public Stack() {
+        this.stack = new ArrayList<>();
+    }
 }
