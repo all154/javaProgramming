cbc7d63ac04d1b336cfb47d7dd28301f86b36c99
Aloisio Valerio Jr <aloisio.valeriojr@gmail.com>
2022-05-04 11:59:17 -0300

    feat: Gift constructor

--- mooc-java-programming-i/part06-Part06_05.SantasWorkshop/src/main/java/Gift.java
+++ mooc-java-programming-i/part06-Part06_05.SantasWorkshop/src/main/java/Gift.java
@@ -1,13 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author alois
- */
 public class Gift {
+    private String name;
+    private int weight;
+    
+    public Gift(String name, int weight) {
+        this.name = name;
+        this.weight = weight;
+    }
     
 }
