ae2b81e74371486d7d897b90c6207fddccaf5983
Aloisio Valerio Jr <aloisio.valeriojr@gmail.com>
2022-05-04 11:25:28 -0300

    feat: MessagingService constructor

--- mooc-java-programming-i/part06-Part06_03.MessagingService/src/main/java/MessagingService.java
+++ mooc-java-programming-i/part06-Part06_03.MessagingService/src/main/java/MessagingService.java
@@ -1,13 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author alois
- */
+import java.util.ArrayList;
+
+
 public class MessagingService {
-    
+    private ArrayList<Message> messages;
+
+    public MessagingService() {
+        this.messages = new ArrayList<>();
+    }
 }
