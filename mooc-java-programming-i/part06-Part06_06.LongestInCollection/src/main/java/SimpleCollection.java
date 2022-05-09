2a42ff52489bf34a6bd28a9134a6d029eb1e9c7c
Aloisio Valerio Jr <aloisio.valeriojr@gmail.com>
2022-05-09 14:48:34 -0300

    feat: longest()

--- mooc-java-programming-i/part06-Part06_06.LongestInCollection/src/main/java/SimpleCollection.java
+++ mooc-java-programming-i/part06-Part06_06.LongestInCollection/src/main/java/SimpleCollection.java
@@ -18,5 +18,23 @@ public class SimpleCollection {
     public ArrayList<String> getElements() {
         return this.elements;
     }
+    
+    public String longest() {
+        if (this.elements.isEmpty()) {
+        return null;
+        }
+        
+        String returnString = "";
+        int longestStringLength = 0;
+        
+        for (String element: this.elements) {
+            if (element.length() > longestStringLength) {
+                longestStringLength = element.length();
+                returnString = element;
+            }
+        }        
+        
+        return returnString;
+    }
 
 }
