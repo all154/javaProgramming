481a280580d9fc5d43718f64363857bee6edef44
Aloisio Valerio Jr <aloisio.valeriojr@gmail.com>
2022-05-10 10:39:53 -0300

    add: Suitcase class with constructor

---
+++ mooc-java-programming-i/part06-Part06_08.CargoHold/src/main/java/Suitcase.java
@@ -0,0 +1,14 @@
+
+import java.util.ArrayList;
+
+
+public class Suitcase {
+    private ArrayList<Item> items;
+    private int maximumWeight;
+    
+    public Suitcase(int maximumWeight) {
+        this.items = new ArrayList<> ();
+        this.maximumWeight = maximumWeight;
+    }
+    
+}
