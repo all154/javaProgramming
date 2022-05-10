d79fb551d2ee79ca8c27c32ebecd3bcef7701106
Aloisio Valerio Jr <aloisio.valeriojr@gmail.com>
2022-05-10 11:24:13 -0300

    fix: Hold constructor

--- mooc-java-programming-i/part06-Part06_08.CargoHold/src/main/java/Hold.java
+++ mooc-java-programming-i/part06-Part06_08.CargoHold/src/main/java/Hold.java
@@ -1,8 +1,13 @@
 
+import java.util.ArrayList;
+
+
 public class Hold {
     private int maximumWeight;
+    private ArrayList<Suitcase> suitcases;
     
     public Hold(int maximumWeight) {
         this.maximumWeight = maximumWeight;
+        this.suitcases = new ArrayList<> ();
     }
 }
