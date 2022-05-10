8f911944c96fa6c290eb3deb79908aa161de3271
Aloisio Valerio Jr <aloisio.valeriojr@gmail.com>
2022-05-10 10:29:56 -0300

    feat: Item class constructor

--- mooc-java-programming-i/part06-Part06_08.CargoHold/src/main/java/Item.java
+++ mooc-java-programming-i/part06-Part06_08.CargoHold/src/main/java/Item.java
@@ -1,4 +1,11 @@
 
 public class Item {
+    private String name;
+    private int weight;
+    
+    public Item(String name, int weight) {
+        this.name = name;
+        this.weight = weight;
+    }
     
 }
