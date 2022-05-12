0aa85d0bb43ee45e3722d4b97252cbc1415fea76
Aloisio Valerio Jr <aloisio.valeriojr@gmail.com>
2022-05-12 12:30:36 -0300

    feat: add UserInterface constructor

--- mooc-java-programming-i/part06-Part06_10.TodoList/src/main/java/UserInterface.java
+++ mooc-java-programming-i/part06-Part06_10.TodoList/src/main/java/UserInterface.java
@@ -1,4 +1,13 @@
 
+import java.util.Scanner;
+
+
 public class UserInterface {
+    private Scanner scanner;
+    private TodoList list;
     
+    public UserInterface(TodoList list, Scanner scanner) {
+        this.list = list;
+        this.scanner = scanner;
+    }
 }
