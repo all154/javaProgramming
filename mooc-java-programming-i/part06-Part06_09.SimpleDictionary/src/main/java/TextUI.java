4bace5fe90b8670b62b65ac376eb1095ae7bdc74
Aloisio Valerio Jr <aloisio.valeriojr@gmail.com>
2022-05-12 11:24:44 -0300

    feat: add TextUI constructor

--- mooc-java-programming-i/part06-Part06_09.SimpleDictionary/src/main/java/TextUI.java
+++ mooc-java-programming-i/part06-Part06_09.SimpleDictionary/src/main/java/TextUI.java
@@ -1,4 +1,14 @@
 
+import java.util.Scanner;
+
+
 public class TextUI {
+    private Scanner scanner;
+    private SimpleDictionary dictionary;
+    
+    public TextUI(Scanner scanner, SimpleDictionary dictionary) {
+        this.scanner = scanner;
+        this.dictionary = dictionary;
+    }
     
 }
