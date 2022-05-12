f3645cb642a4bb2b77b45660f4c8d2e65029b98e
Aloisio Valerio Jr <aloisio.valeriojr@gmail.com>
2022-05-12 12:41:00 -0300

    feat: add start method

--- mooc-java-programming-i/part06-Part06_10.TodoList/src/main/java/UserInterface.java
+++ mooc-java-programming-i/part06-Part06_10.TodoList/src/main/java/UserInterface.java
@@ -10,4 +10,23 @@ public class UserInterface {
         this.list = list;
         this.scanner = scanner;
     }
+    
+    public void start(){
+        while(true){
+            System.out.println("Command: ");
+            String command = scanner.nextLine();
+            
+            if (command.equals("stop")){
+                break;
+            } else if (command.equals("add")) {
+                this.list.add(command);
+            } else if (command.equals("list")) {
+                this.list.print();
+            } else if (command.equals("remove")) {
+                System.out.println("Which one is removed?");
+                int index = Integer.valueOf(scanner.nextLine());
+                this.list.remove(index);
+            }
+        }
+    }
 }
