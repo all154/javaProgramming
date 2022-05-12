fe8690e86d67253f996fa895f9ec3516dc7a248d
Aloisio Valerio Jr <aloisio.valeriojr@gmail.com>
2022-05-12 11:36:50 -0300

    feat: add start

--- mooc-java-programming-i/part06-Part06_09.SimpleDictionary/src/main/java/TextUI.java
+++ mooc-java-programming-i/part06-Part06_09.SimpleDictionary/src/main/java/TextUI.java
@@ -11,4 +11,24 @@ public class TextUI {
         this.dictionary = dictionary;
     }
     
+    public void start() {
+        while (true) {
+            System.out.println("Command:");
+            String command = scanner.nextLine();
+            
+            if (command.equals("end")) {
+                System.out.println("Bye bye!");
+                break;
+            } else if (command.equals("add")) {
+                System.out.println("Word:");
+                String word = scanner.nextLine();
+                System.out.println("Translation:");
+                String translation = scanner.nextLine();
+                this.dictionary.add(word, translation);
+            } else {
+                System.out.println("Unknown command");
+            }
+        }
+    }
+    
 }
