f9be063cc42b3b6a3e8aa376290f52a50534947a
Aloisio Valerio Jr <aloisio.valeriojr@gmail.com>
2022-05-19 14:20:51 -0300

    feat: add UserInterface constructor

--- mooc-java-programming-i/part06-Part06_12.JokeManager/src/main/java/UserInterface.java
+++ mooc-java-programming-i/part06-Part06_12.JokeManager/src/main/java/UserInterface.java
@@ -1,4 +1,13 @@
 
+import java.util.Scanner;
+
+
 public class UserInterface {
+    private JokeManager jokes;
+    private Scanner scanner;
     
+    public UserInterface(JokeManager jokes, Scanner scanner) {
+        this.jokes = jokes;
+        this.scanner = scanner;
+    }
 }
