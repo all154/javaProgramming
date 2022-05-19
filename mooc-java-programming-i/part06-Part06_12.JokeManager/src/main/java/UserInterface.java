a1ad94d4ca8733678b5ee268938b8a2f2e0a3aa4
Aloisio Valerio Jr <aloisio.valeriojr@gmail.com>
2022-05-19 14:29:50 -0300

    feat: add method that starts the user interface

--- mooc-java-programming-i/part06-Part06_12.JokeManager/src/main/java/UserInterface.java
+++ mooc-java-programming-i/part06-Part06_12.JokeManager/src/main/java/UserInterface.java
@@ -10,4 +10,30 @@ public class UserInterface {
         this.jokes = jokes;
         this.scanner = scanner;
     }
+    
+    public void start() {
+        while (true) {
+            System.out.println("Commands:");
+            System.out.println(" 1 - add a joke");
+            System.out.println(" 2 - draw a joke");
+            System.out.println(" 3 - list jokes");
+            System.out.println(" X - stop");
+            
+            String command = scanner.nextLine();
+            
+            if (command.equals("X")) {
+                break;
+            } else if (command.equals("1")) {
+                System.out.println("Write the joke to be added:");
+                String joke = scanner.nextLine();
+                this.jokes.addJoke(joke);
+            } else if (command.equals("2")) {
+                System.out.println("Drawing a joke.");
+                System.out.println(this.jokes.drawJoke());
+            } else if (command.equals("3")) {
+                System.out.println("Printing the jokes.");
+                this.jokes.printJokes();
+            }
+        }
+    }
 }
