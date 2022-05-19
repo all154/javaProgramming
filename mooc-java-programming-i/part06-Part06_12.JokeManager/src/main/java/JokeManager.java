f12f208bd7b5dacf07fcca489aa0fce4c2296e4c
Aloisio Valerio Jr <aloisio.valeriojr@gmail.com>
2022-05-19 14:01:24 -0300

    feat: add JokeManager constructor and private variables

--- mooc-java-programming-i/part06-Part06_12.JokeManager/src/main/java/JokeManager.java
+++ mooc-java-programming-i/part06-Part06_12.JokeManager/src/main/java/JokeManager.java
@@ -1,4 +1,11 @@
 
+import java.util.ArrayList;
+
+
 public class JokeManager {
+    private ArrayList<String> jokes;
     
+    public JokeManager() {
+        this.jokes = new ArrayList<>();
+    }
 }
