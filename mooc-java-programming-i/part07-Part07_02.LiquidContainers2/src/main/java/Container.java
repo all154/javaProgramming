396b8e9044ca1a70799e7a0c846d846949756b26
Aloisio Valerio Jr <aloisio.valeriojr@gmail.com>
2022-05-25 11:18:40 -0300

    feat: add method that adds amount to Container

--- mooc-java-programming-i/part07-Part07_02.LiquidContainers2/src/main/java/Container.java
+++ mooc-java-programming-i/part07-Part07_02.LiquidContainers2/src/main/java/Container.java
@@ -1,13 +1,24 @@
 
 public class Container {
+
     private int contains;
-    
+
     public Container() {
         this.contains = 0;
     }
-    
+
     public int contains() {
         return this.contains;
     }
-    
+
+    public void add(int amount) {
+        if (amount > 0) {
+            if (this.contains + amount < 100) {
+                this.contains += amount;
+            } else {
+                this.contains = 100;
+            }
+        }
+    }
+
 }
