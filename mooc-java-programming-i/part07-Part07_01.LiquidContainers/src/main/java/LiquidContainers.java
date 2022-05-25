8dcbd2fb7667cd37a4252ada42d029a4757190ab
Aloisio Valerio Jr <aloisio.valeriojr@gmail.com>
2022-05-25 11:08:16 -0300

    feat: add commands 'add', 'move' and 'remove' to main program

--- mooc-java-programming-i/part07-Part07_01.LiquidContainers/src/main/java/LiquidContainers.java
+++ mooc-java-programming-i/part07-Part07_01.LiquidContainers/src/main/java/LiquidContainers.java
@@ -5,14 +5,52 @@ public class LiquidContainers {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
+        int first = 0;
+        int second = 0;
 
         while (true) {
+            System.out.println("First: " + first + "/100");
+            System.out.println("Second: " + second + "/100");
             System.out.print("> ");
 
             String input = scan.nextLine();
             if (input.equals("quit")) {
                 break;
+            }
+            
+            String[] parts = input.split(" ");
+
+            String command = parts[0];
+            int amount = Integer.valueOf(parts[1]);
+            
+            if (command.equals("add") && amount > 0) {
+                if (first + amount < 100) {
+                    first += amount;
+                } else {
+                    first = 100;
+                }
+            }
+            
+            if (command.equals("move") && amount >= 0) {
+                if (first - amount < 0) {
+                    second += first;
+                    first = 0;
+                } else {
+                    first -= amount;
+                    if (second + amount > 100) {
+                        second = 100;
+                    } else {
+                        second += amount;
+                    }
+                }  
+            }
+            
+            if (command.equals("remove") && amount > 0) {
+                if (second - amount > 0) {
+                    second -= amount;
+                } else {
+                    second = 0;
+                }
             }
 
         }
