85de02eee0586997bb38d3cff72c67196567a54c
Aloisio Valerio Jr <aloisio.valeriojr@gmail.com>
2022-06-01 11:54:14 -0300

    feat: add sort(int[] array)

--- mooc-java-programming-i/part07-Part07_04.ReadymadeSortingAlgorithms/src/main/java/Main.java
+++ mooc-java-programming-i/part07-Part07_04.ReadymadeSortingAlgorithms/src/main/java/Main.java
@@ -1,10 +1,17 @@
 
+import java.util.Arrays;
+
+
 
 public class Main {
 
     public static void main(String[] args) {
         // insert test code here
         int[] array = {3, 1, 5, 99, 3, 12};
+    }
+    
+    public static void sort(int[] array) {
+        Arrays.sort(array);
     }
 
 }
