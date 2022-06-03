3b0a2585cd6780ab7cad367b5f5d2f57910b6ec9
Aloisio Valerio Jr <aloisio.valeriojr@gmail.com>
2022-06-03 12:04:36 -0300

    feat: add Points constructor

--- mooc-java-programming-i/part07-Part07_06.GradeStatistics/src/main/java/Points.java
+++ mooc-java-programming-i/part07-Part07_06.GradeStatistics/src/main/java/Points.java
@@ -1,4 +1,12 @@
 
+import java.util.ArrayList;
+
+
 public class Points {
+    private ArrayList<Integer> points;
+    
+    public Points() {
+        this.points = new ArrayList<>();
+    }
     
 }
