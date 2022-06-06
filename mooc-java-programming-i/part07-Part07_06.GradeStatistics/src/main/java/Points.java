bdafa3ccb30126926ab857a77ea7140dcbb1f0ae
Aloisio Valerio Jr <aloisio.valeriojr@gmail.com>
2022-06-06 13:56:01 -0300

    feat: add gradeDistribution

--- mooc-java-programming-i/part07-Part07_06.GradeStatistics/src/main/java/Points.java
+++ mooc-java-programming-i/part07-Part07_06.GradeStatistics/src/main/java/Points.java
@@ -56,4 +56,73 @@ public class Points {
         return 100*(double)passed/this.points.size();
     }
     
+    public void gradeDistribution() {
+        String star = "*";
+        int grade0 = 0;
+        int grade1 = 0;
+        int grade2 = 0;
+        int grade3 = 0;
+        int grade4 = 0;
+        int grade5 = 0;
+        
+        for (int point: this.points){
+            if (point < 50) {
+                grade0 += 1;
+            } else if (point < 60) {
+                grade1 += 1;
+            } else if (point < 70) {
+                grade2 += 1;
+            } else if (point < 80) {
+                grade3 += 1;
+            } else if (point < 90) {
+                grade4 += 1;
+            } else {
+                grade5 += 1;
+            }
+        }
+        
+        System.out.println("Grade distribution:");
+        System.out.print("5: ");
+        
+        for (int i = 0; i < grade5; i++) {
+            System.out.print("*");
+        }
+        System.out.println("");
+        
+        System.out.print("4: ");
+        
+        for (int i = 0; i < grade4; i++) {
+            System.out.print("*");
+        }
+        System.out.println("");
+        
+        System.out.print("3: ");
+        
+        for (int i = 0; i < grade3; i++) {
+            System.out.print("*");
+        }
+        System.out.println("");
+        
+        System.out.print("2: ");
+        
+        for (int i = 0; i < grade2; i++) {
+            System.out.print("*");
+        }
+        System.out.println("");
+        
+        System.out.print("1: ");
+        
+        for (int i = 0; i < grade1; i++) {
+            System.out.print("*");
+        }
+        System.out.println("");
+        
+        System.out.print("0: ");
+        
+        for (int i = 0; i < grade0; i++) {
+            System.out.print("*");
+        }
+        System.out.println("");
+    }
+    
 }
