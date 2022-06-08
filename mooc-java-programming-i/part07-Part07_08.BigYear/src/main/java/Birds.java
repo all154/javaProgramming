b374d0147aeb9ff4951b482068ac8343665e3a2a
Aloisio Valerio Jr <aloisio.valeriojr@gmail.com>
2022-06-08 11:56:16 -0300

    feat: add method that adds a bird to the list

--- mooc-java-programming-i/part07-Part07_08.BigYear/src/main/java/Birds.java
+++ mooc-java-programming-i/part07-Part07_08.BigYear/src/main/java/Birds.java
@@ -1,5 +1,6 @@
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 
 public class Birds {
@@ -7,6 +8,15 @@ public class Birds {
     
     public Birds(){
         this.birds = new ArrayList();
+    }
+    
+    public void add(Scanner scan){
+        System.out.println("Name:");
+        String name = scan.nextLine();
+        System.out.println("Name in Latin:");
+        String latinName = scan.nextLine();
+        
+        this.birds.add(new Bird(name, latinName));
     }
     
 }
