e12d773c87b556364d0f52429cd9ba843506df9d
Aloisio Valerio Jr <aloisio.valeriojr@gmail.com>
2022-06-08 12:22:36 -0300

    fix: add while loop in start

--- mooc-java-programming-i/part07-Part07_08.BigYear/src/main/java/UserInterface.java
+++ mooc-java-programming-i/part07-Part07_08.BigYear/src/main/java/UserInterface.java
@@ -1,33 +1,37 @@
 
 import java.util.Scanner;
 
-
 public class UserInterface {
+
     private Scanner scanner;
 
     public UserInterface(Scanner scanner) {
         this.scanner = scanner;
     }
-    
+
     public void start() {
         Birds birds = new Birds();
-        
-        System.out.println("Command: ");
-        String command = scanner.nextLine();
-        
-        if (command.equals("Add")) {
-            birds.add(scanner);
-        }
-        
-        if (command.equals("Observation")) {
-            birds.observation(scanner);
-        }
-        
-        if (command.equals("All")) {
-            birds.all();
-        }
 
-        
+        while (true) {
+            System.out.println("Command: ");
+            String command = scanner.nextLine();
+
+            if (command.equals("Add")) {
+                birds.add(scanner);
+            }
+
+            if (command.equals("Observation")) {
+                birds.observation(scanner);
+            }
+
+            if (command.equals("All")) {
+                birds.all();
+            }
+
+            if (command.equals("Quit")) {
+                break;
+            }
+        }
     }
 
 }
