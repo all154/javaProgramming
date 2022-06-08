6e9020c250fb5f9f3243a9375ca993bb4ff9fa4d
Aloisio Valerio Jr <aloisio.valeriojr@gmail.com>
2022-06-08 10:47:38 -0300

    feat: add Recipe class constructor

--- mooc-java-programming-i/part07-Part07_07.RecipeSearch/src/main/java/Recipe.java
+++ mooc-java-programming-i/part07-Part07_07.RecipeSearch/src/main/java/Recipe.java
@@ -1,13 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author alois
- */
+import java.util.ArrayList;
+
+
 public class Recipe {
+    private String title;
+    private int cookingTime;
+    private ArrayList<String> ingredients;
+    
+    public Recipe(String title, int cookingTime, ArrayList ingredients) {
+        this.title = title;
+        this.cookingTime = cookingTime;
+        this.ingredients = ingredients;
+    }
     
 }
