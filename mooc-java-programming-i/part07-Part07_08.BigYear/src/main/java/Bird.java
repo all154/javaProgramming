0650c5190a7a8b54f39d66dc1c5818d43f7a7826
Aloisio Valerio Jr <aloisio.valeriojr@gmail.com>
2022-06-08 11:55:30 -0300

    feat: add Bird class constructor

--- mooc-java-programming-i/part07-Part07_08.BigYear/src/main/java/Bird.java
+++ mooc-java-programming-i/part07-Part07_08.BigYear/src/main/java/Bird.java
@@ -1,4 +1,12 @@
 
 public class Bird {
+    private String name;
+    private String latinName;
+    private int observations;
     
+    public Bird(String name, String latinName) {
+        this.name = name;
+        this.latinName = latinName;
+        this.observations = 0;
+    }
 }
