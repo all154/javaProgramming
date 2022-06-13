791628a639340cd997d8339b70f21f210a6d7585
Aloisio Valerio Jr <aloisio.valeriojr@gmail.com>
2022-06-13 11:01:13 -0300

    fix: stop skipping last recipe in readFile

--- mooc-java-programming-i/part07-Part07_07.RecipeSearch/src/main/java/UserInterface.java
+++ mooc-java-programming-i/part07-Part07_07.RecipeSearch/src/main/java/UserInterface.java
@@ -21,7 +21,7 @@ public class UserInterface {
                 + "stop - stops the program\n"
                 + "find name - searches recipes by name\n"
                 + "find cooking time - searches recipes by cooking time");
-        
+
         while (true) {
             System.out.println("Enter command: ");
             String command = scanner.nextLine();
@@ -29,7 +29,7 @@ public class UserInterface {
             if (command.equals("list")) {
                 listOfRecipes.list();
             }
-            
+
             if (command.equals("stop")) {
                 break;
             }
@@ -68,5 +68,15 @@ public class UserInterface {
         } catch (Exception e) {
             System.out.println("Error: " + e.getMessage());
         }
+
+        String title = list.get(0);
+        int cookingTime = Integer.valueOf(list.get(1));
+
+        for (int i = 2; i < list.size(); i++) {
+            ingredients.add(list.get(i));
+        }
+
+        Recipe newRecipe = new Recipe(title, cookingTime, ingredients);
+        listOfRecipes.add(newRecipe);
     }
 }
