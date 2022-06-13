ef6fc7a9efa2078edd39bb35e5d3e774db6acd28
Aloisio Valerio Jr <aloisio.valeriojr@gmail.com>
2022-06-13 11:49:43 -0300

    feat: add 'find ingredient' command

--- mooc-java-programming-i/part07-Part07_07.RecipeSearch/src/main/java/UserInterface.java
+++ mooc-java-programming-i/part07-Part07_07.RecipeSearch/src/main/java/UserInterface.java
@@ -20,7 +20,8 @@ public class UserInterface {
                 + "list - lists the recipes\n"
                 + "stop - stops the program\n"
                 + "find name - searches recipes by name\n"
-                + "find cooking time - searches recipes by cooking time");
+                + "find cooking time - searches recipes by cooking time\n"
+                + "find ingredient - searches recipes by ingredient");
 
         while (true) {
             System.out.print("Enter command: ");
@@ -29,17 +30,23 @@ public class UserInterface {
             if (command.equals("list")) {
                 listOfRecipes.list();
             }
-            
+
             if (command.equals("find name")) {
                 System.out.print("Searched word: ");
                 String word = scanner.nextLine();
                 listOfRecipes.findName(word);
             }
-            
+
             if (command.equals("find cooking time")) {
                 System.out.print("Max cooking time: ");
                 int maxCookingTime = Integer.valueOf(scanner.nextLine());
                 listOfRecipes.findCookingTime(maxCookingTime);
+            }
+            
+            if (command.equals("find ingredient")) {
+                System.out.print("Ingredient: ");
+                String ingredient = scanner.nextLine();
+                listOfRecipes.findIngredient(ingredient);
             }
 
             if (command.equals("stop")) {
