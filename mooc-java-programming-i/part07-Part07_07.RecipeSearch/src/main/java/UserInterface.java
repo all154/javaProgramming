7103cdd8743a719234fa4093cc14432b1ad399bd
Aloisio Valerio Jr <aloisio.valeriojr@gmail.com>
2022-06-13 10:29:02 -0300

    feat: add start and readFile methods

--- mooc-java-programming-i/part07-Part07_07.RecipeSearch/src/main/java/UserInterface.java
+++ mooc-java-programming-i/part07-Part07_07.RecipeSearch/src/main/java/UserInterface.java
@@ -1,11 +1,60 @@
 
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Scanner;
 
-
 public class UserInterface {
+
     private Scanner scanner;
 
     public UserInterface(Scanner scanner) {
         this.scanner = scanner;
+    }
+
+    public void start() {
+        ListOfRecipes listOfRecipes = new ListOfRecipes();
+
+        this.readFile(listOfRecipes);
+
+        System.out.println("Commands:\n"
+                + "list - lists the recipes\n"
+                + "stop - stops the program\n"
+                + "find name - searches recipes by name\n"
+                + "find cooking time - searches recipes by cooking time");
+        System.out.println("Enter command: ");
+
+    }
+
+    public void readFile(ListOfRecipes listOfRecipes) {
+        System.out.print("File to read: ");
+        String file = scanner.nextLine();
+
+        ArrayList<String> list = new ArrayList<>();
+        ArrayList<String> ingredients = new ArrayList<>();
+
+        try (Scanner scan = new Scanner(Paths.get(file))) {
+            while (scan.hasNextLine()) {
+                String row = scan.nextLine();
+
+                if (row.isEmpty()) {
+                    String title = list.get(0);
+                    int cookingTime = Integer.valueOf(list.get(1));
+
+                    for (int i = 2; i < list.size(); i++) {
+                        ingredients.add(list.get(i));
+                    }
+
+                    Recipe newRecipe = new Recipe(title, cookingTime, ingredients);
+                    listOfRecipes.add(newRecipe);
+
+                    list.clear();
+                    ingredients.clear();
+                } else {
+                    list.add(row);
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 }
