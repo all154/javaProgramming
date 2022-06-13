c95c34db832f57ab123d64c635efeeb19b203b03
Aloisio Valerio Jr <aloisio.valeriojr@gmail.com>
2022-06-13 10:18:04 -0300

    add: add ListOfRecipes class

---
+++ mooc-java-programming-i/part07-Part07_07.RecipeSearch/src/main/java/ListOfRecipes.java
@@ -0,0 +1,11 @@
+
+import java.util.ArrayList;
+
+
+public class ListOfRecipes {
+    private ArrayList<Recipe> listOfRecipes;
+    
+    public ListOfRecipes(){
+        this.listOfRecipes = new ArrayList();
+    }
+}
